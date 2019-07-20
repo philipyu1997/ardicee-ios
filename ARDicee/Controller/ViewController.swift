1a5877a1a571d793e29fe9446aec14df07df73a2
Philip Yu <pyu3@mail.sfsu.edu>
2019-07-19 20:07:57 -0700

    Create a 3D Object in Augmented Reality

--- ARDicee/Controller/ViewController.swift
+++ ARDicee/Controller/ViewController.swift
@@ -20,14 +20,25 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+        let material = SCNMaterial()
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        material.diffuse.contents = UIColor.red
+        cube.materials = [material]
         
-        // Set the scene to the view
-        sceneView.scene = scene
+        let node = SCNNode()
+        
+        node.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
+        node.geometry = cube
+        
+        sceneView.scene.rootNode.addChildNode(node)
+        sceneView.autoenablesDefaultLighting = true
+        
+//        // Create a new scene
+//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+//
+//        // Set the scene to the view
+//        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
